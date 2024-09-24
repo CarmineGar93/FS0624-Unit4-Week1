268c9cd7c3231ada8dc5e9fb0f4dd14330734e5b
Carmy93 <carmygargiulo45@gmail.com>
2024-09-24 14:29:11 +0200

    1.1 reviewed

--- Day 2/src/If_Else/Esercizio1.java
+++ Day 2/src/If_Else/Esercizio1.java
@@ -2,7 +2,6 @@ package If_Else;
 
 public class Esercizio1 {
     public static boolean pariDispari(String str){
-        if(str.length() % 2 == 0) return true;
-        else return false;
+        return str.length() % 2 == 0;
     }
 }
