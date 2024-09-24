15aef1f85fc37655b3ab80072e99fcc627e4e49e
Carmy93 <carmygargiulo45@gmail.com>
2024-09-24 15:45:46 +0200

    4.0 reviewed

--- Day 2/src/For/Esercizio4.java
+++ Day 2/src/For/Esercizio4.java
@@ -6,10 +6,15 @@ public class Esercizio4 {
     public static void countdown(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Inserisci un numero per iniziare il countdown");
-        int begin = sc.nextInt();
-        for (int i = begin; i >= 0 ; i--) {
-            System.out.println(i);
+        try{
+            int begin = sc.nextInt();
+            for (int i = begin; i >= 0 ; i--) {
+                System.out.println(i);
+            }
+            System.out.println("Auguriiii");
+        } catch (Exception e){
+            System.out.println("Riprova");
+            countdown();
         }
-        System.out.println("Auguriiii");
     }
 }
