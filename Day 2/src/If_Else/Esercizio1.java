cf16912aee7f36cd0cd455f13578453e689b01b8
Carmy93 <carmygargiulo45@gmail.com>
2024-09-24 14:23:23 +0200

    1.1

---
+++ Day 2/src/If_Else/Esercizio1.java
@@ -0,0 +1,8 @@
+package If_Else;
+
+public class Esercizio1 {
+    public static boolean pariDispari(String str){
+        if(str.length() % 2 == 0) return true;
+        else return false;
+    }
+}
--- Day 2/src/Main.java
+++ Day 2/src/Main.java
@@ -1,5 +1,17 @@
+import If_Else.Esercizio1;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("---------Esercizio 1----------");
+        System.out.println("-------------1.1--------------");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Inserisci una stringa per verificare se la sua lunghezza è pari o dispari");
+        String str = sc.nextLine();
+        boolean verifica = Esercizio1.pariDispari(str);
+        if(verifica) System.out.println("La stringa è pari");
+        else System.out.println("La stringa è dispari");
+
     }
 }
